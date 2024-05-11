a26d05eab879b56a59610a0f43ef03fea076e0d9
wcoee <132860485+wcoee@users.noreply.github.com>
2024-05-11 22:35:32 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          © Hello.un.edu.pl
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -217,9 +217,9 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
+            //<a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+              //<IconButton icon={<GithubIcon />} shadow />
+            //</a>
           </div>
         </div>
         <div>
